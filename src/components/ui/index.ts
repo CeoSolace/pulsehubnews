4b26110d867b21ee9253af5a13fc79d8ffde61c7
Solace <monkdev9@gmail.com>
2025-12-12 00:44:53 +0000

    Create index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+// src/components/ui/index.ts
+export { Button } from './button';
+export { Input } from './input';
+export { Label } from './label';
+export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select';
