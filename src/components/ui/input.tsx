861b3f5c982d7881768ce849fd383ddb955facc7
Solace <monkdev9@gmail.com>
2025-12-12 01:22:12 +0000

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,20 +1,7 @@
-// src/components/ui/input.tsx
-'use client';
-
-import * as React from 'react';
-import { cn } from '@/lib/utils';
-
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-export function Input({ className, type = 'text', ...props }: InputProps) {
+export function Input({ className = '', ...props }) {
   return (
     <input
-      type={type}
-      className={cn(
-        'flex h-10 w-full rounded-md border border-gray bg-background px-3 py-2 text-sm',
-        'focus:outline-none focus:ring-1 focus:ring-blue disabled:cursor-not-allowed disabled:opacity-50',
-        className
-      )}
+      className={`px-3 py-2 border border-gray rounded w-full ${className}`}
       {...props}
     />
   );
